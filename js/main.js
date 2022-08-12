49d0ee6afa9a467165fe0186e9c31ae2f4dd220c
mrgarcia12 <104576878+mrgarcia12@users.noreply.github.com>
2022-08-12 09:52:11 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+let frutas = ["🍇", "🍒", "🥝", "🍉", "🫐", "🍎", "🍐", "🍓","🍈","🍇", "🍒", "🥝", "🍉", "🫐", "🍎", "🍐", "🍓","🍈"];
+
+
+let root = document.querySelector("#root");
+
+//FUNCION AGREGAR AL DOM
+
+function agregar_al_DOM(){
+
+    frutas.forEach(function (elemento, indice){
+
+        let contenedor = document.createElement("div");
+
+    contenedor.innerHTML = 
+    '<div class="fruta">' + 
+    elemento + 
+    '</div>';
+
+
+    root.appendChild(contenedor); 
+        
+        });
+
+    //ford
+
+}
+
+agregar_al_DOM();
+
+//FUNCION ACTIVAR LAS LETRAS
+
+let datos_del_don = document.querySelectorAll(".fruta");
+
+function activar(){
+    this.classList.add("activo");
+
+    
+
+}
+
+datos_del_don.forEach(function(elemento, indice){
+    
+    elemento.addEventListener('click', activar )
+});
